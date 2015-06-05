059746e9cc9405b71d2c7934937dfc8f350b39e9
MikeElsers <MikeElsers@users.noreply.github.com>
2015-06-05 06:41:59 +0200

    solution for task 1 part 2:  plot2.R  and plot2.png -- ready for submission

---
+++ plot2.R
@@ -0,0 +1,85 @@
+##  week 01:   plot examples
+
+
+#---------- plot 2 function  ------------------------------------
+
+plot2    <-  function (){
+
+    
+    #------------ set the file to be used -----------------------
+    #-----------  (..._reduced  only for test purpose) ----------
+    filename <-  "household_power_consumption.txt"
+    #filename <-  "household_power_consumption__reduced.txt"
+    
+    
+    alldata <- read.csv( filename,  sep = c(';')) 
+    
+    
+    ##------------ convert date  and time --------------
+    cmbDate <-  paste (  alldata$Date, "_", alldata$Time, sep="")
+
+    alldata$Date  <-   as.Date( alldata$Date,  "%d/%m/%Y")
+    alldata$FullDate  <-  strptime ( cmbDate ,  "%d/%m/%Y_%H:%M:%S"  )
+    
+    lowDate <-  as.Date ("2007-02-01", "%Y-%m-%d")
+    highDate <- as.Date ("2007-02-02", "%Y-%m-%d")
+
+    #---------- filter which data to be used ----------------------------
+    data  <-  subset( alldata, 
+                      (alldata$Date >= lowDate) & (alldata$Date <= highDate)  )
+
+    
+    #----------  convert G_act_pow to numeric ---------------------------
+    #---------   (done after reducing the number of rows --> faster) ----
+    data$Global_active_power  <-  
+                          as.numeric(as.character(data$Global_active_power))
+
+
+    #------------ modify the parameters  -----------------------------------
+    
+    #par()              # view current settings
+    opar <- par()      # make a copy of current settings
+    par(lty=1)         # use a solid line 
+
+
+    # -----------  do empty plot -----------------------------------
+    plot (  
+             data$FullDate , 
+        data$Global_active_power, 
+        xlab = "", 
+        ylab = "Global Active Power (kilowatts)"
+        , type ="n"
+        )
+
+    
+    #----------  now draw the lines  --------------------------------
+    lines (  
+        data$FullDate , 
+        data$Global_active_power 
+    )
+
+    
+    #---------   create the plot output  (PNG file) ----------------
+    dev.copy (png,file = "plot2.png")
+    dev.off()
+
+
+    #----------   restore the parameters -------------------------
+
+    par(opar)          # restore original settings
+
+
+}
+
+##==============================================================================
+
+test  <-  function (){
+    print ("")
+    print (" --------- starting plot2 TEST  function --------------------")
+    print ("")
+    print (plot2())
+}
+
+
+##  start the test
+test()
